9a1ecf47c337350f9b1072fb43482f0d6a66eae0
Ariana Hall <amhall4323@ung.edu>
2019-05-16 22:53:16 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function inc(n) {
+  number += n
+}
+function dec(n) {
+  number -= n
+}
+function add(a, b) {
+  return a + b
+}
+function subtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a*b
+}
+function divide(a, b) {
+  return a / b
+}
+function makeInt(n) {
+  return parseInt('n', 10)
+}
